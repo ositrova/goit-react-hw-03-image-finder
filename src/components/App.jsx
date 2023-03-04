14f7c675ebe9f165b9c3c4c273f1311aa22d6f38
ositrova <110326755+ositrova@users.noreply.github.com>
2023-03-04 16:21:19 +0200

    one part task

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,75 @@
-export const App = () => {
+
+import { Component } from "react";
+import { fetchPictures } from "services/gallaryAPI";
+import { GlobalStyle } from "./GlobalStyle";
+import { ImageGallery } from "./ImageGallery/ImageGallery";
+import { Layout } from "./Layout/Layout";
+import { Modal } from "./Modal/Modal";
+import { Searchbar } from "./Searchbar/Searchbar";
+
+export class App extends Component {
+  state = {
+    pictures: [],
+    status: 'idle',
+    showModal: false,
+    page: 1,
+    query: '',
+    tags: '',
+    largeImageUrl: '',
+    loadMore: null,
+  };
+
+  searchResult = value => {
+    this.setState({ query: value, page: 1, pictures: [], loadMore: null});
+  };
+
+toggleModal = () => {
+  this.setState(({showModal}) => ({
+    showModal: !showModal,
+  }))
+};
+
+getLargeImg = imgUrl => {
+  this.setState({largeImageUrl: imgUrl})
+  this.toggleModal()
+};
+
+componentDidUpdate(_, prevState) {
+  const {page, query} = this.state;
+
+  if (prevState.page !== page || prevState.query !== query) {
+    this.setState({ status: 'loading'});
+
+    fetchPictures (query, page)
+    .then (e =>
+      this.setState(prevState => ({
+        pictures: [...prevState.pictures, ...e.hits],
+        status: 'idle',
+        loadMore: 12 - e.hits.length,
+      })))
+      .catch(error => console.log(error));
+  }
+};
+
+render(){
+  const {pictures,
+    // status,
+    showModal,
+    // page,
+    // query,
+    tags,
+    largeImageUrl,
+    // loadMore
+  } = this.state
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+    <Layout>
+<GlobalStyle/>
+<Searchbar onSubmit={this.searchResult}/>
+{showModal && (
+  <Modal url={largeImageUrl} alt={tags} onClose={this.toggleModal} />
+)}
+    <ImageGallery pictures={pictures} onClick={this.getLargeImg}/>  
+    </Layout>
   );
 };
+}
---
+++ src/components/Searchbar/Searchbar.jsx
@@ -0,0 +1,62 @@
+import { Component } from 'react';
+import PropTypes from 'prop-types';
+import {
+  SearchbarContainer,
+  SearchForm,
+  SearchFormBtn,
+  SearchFormBtnLabel,
+  SearchFormInput,
+} from './Searchbar.styled';
+
+export class Searchbar extends Component {
+
+  state = {
+    search: '',
+  };
+
+searchResult = e =>{
+  this.setState({
+    search: e.currentTarget.value,
+  })
+};
+
+handleSubmit = e => {
+  e.preventDefault();
+  const {search} = this.state;
+  const {onSubmit} = this.props;
+
+  onSubmit(search);
+  this.setState({
+    search: '',
+  })
+};
+
+  render() {
+const {search} = this.state;
+return (
+
+  <SearchbarContainer>
+  <SearchForm onSubmit={this.handleSubmit}>
+    <SearchFormBtn type="submit">
+      <SearchFormBtnLabel>Search</SearchFormBtnLabel>
+    </SearchFormBtn>
+
+    <SearchFormInput
+      type="text"
+      autoComplete="off"
+      autoFocus
+      placeholder="Search images and photos"
+      value={search}
+      onChange={this.searchResult}
+    />
+  </SearchForm>
+</SearchbarContainer>
+);
+}
+};
+
+
+
+Searchbar.propTypes ={
+  onSubmit: PropTypes.func.isRequired,
+};
